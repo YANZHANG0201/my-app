d3b0f132096a513df94dd26472359e5ecc3d772d
Yan Zhang <149005773+YANZHANG0201@users.noreply.github.com>
2024-10-31 03:43:16 +0000

    user-table

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,12 +1,77 @@
-import React from "react";
-import { Button, Form, Input } from "antd";
+import React, { useEffect, useState } from "react";
+import { Button, Form, Input, Table, Popconfirm } from "antd";
 import "./user.css";
+import { getUser } from "../../api";
 
 const User = () => {
-  const handleClick = () => {};
+  const [listData, setListData] = useState({
+    name: "",
+  });
+  const [TableData, setTableData] = useState([]);
+  const handleClick = (type, rowData) => {};
   const handleFinish = (e) => {
+    setListData({ name: e.name });
     console.log(e);
   };
+  const handleDelete = (rowData) => {};
+
+  const getTableData = () => {
+    getUser(listData).then(({ data }) => {
+      setTableData(data.list);
+    });
+  };
+  const columns = [
+    { title: "name", dataIndex: "name" },
+    {
+      title: "age",
+      dataIndex: "age",
+    },
+    {
+      title: "Gender",
+      dataIndex: "sex",
+      render: (val) => {
+        return val ? "women" : "man";
+      },
+    },
+    {
+      title: "birth",
+      dataIndex: "birth",
+    },
+    {
+      title: "address",
+      dataIndex: "addr",
+    },
+    {
+      title: "Edit",
+      render: (rowData) => {
+        return (
+          <div className="edit-flex">
+            <Button
+              style={{ marginRight: "5px" }}
+              onClick={() => handleClick("edit", rowData)}
+            >
+              Edit
+            </Button>
+            <Popconfirm
+              title="infor"
+              description="This will delete "
+              okText="yes"
+              cancelText="no"
+              onConfirm={() => handleDelete(rowData)}
+            >
+              <Button type="primary" danger>
+                {" "}
+                Delete
+              </Button>
+            </Popconfirm>
+          </div>
+        );
+      },
+    },
+  ];
+  useEffect(() => {
+    getTableData();
+  }, []);
   return (
     <div className="userlist">
       <div className="flex-box space-between">
@@ -23,6 +88,9 @@ const User = () => {
             </Button>
           </Form.Item>
         </Form>
+      </div>
+      <div>
+        <Table columns={columns} dataSource={TableData} rowKey={"id"} />
       </div>
     </div>
   );
